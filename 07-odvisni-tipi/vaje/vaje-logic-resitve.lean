4e26403abb2354f1e8c53a982c638eb960a1854d
Alja Gombač <alja.gombac@gmail.com>
2024-12-02 10:24:04 +0100

    vaje 7

--- 07-odvisni-tipi/vaje/vaje-logic-resitve.lean
+++ 07-odvisni-tipi/vaje/vaje-logic-resitve.lean
@@ -1,7 +1,16 @@
 -- Izomorfizmi
 
 theorem eq1 {A B : Prop} : (A ∧ B) ↔ (B ∧ A) :=
-  sorry
+    by
+        -- apply Iff.intro
+        constructor
+        intro ab
+        -- apply And.intro
+        constructor
+        exact ab.right
+        exact ab.left
+        intro ba
+        sorry
 
 theorem eq2 {A B : Prop} : (A ∨ B) ↔ (B ∨ A) :=
   sorry
@@ -16,7 +25,24 @@ theorem eq5 {A B C : Prop} : A ∧ (B ∨ C) ↔ (A ∧ B) ∨ (A ∧ C) :=
   sorry
 
 theorem eq6 {A B C : Prop} : (B ∨ C) → A ↔ (B → A) ∧ (C → A) :=
-  sorry
+    by
+        constructor
+        intro h
+        constructor
+        intro b
+        apply h
+        left
+        exact b
+        intro c
+        have xx : B ∨ C := Or.inr c
+        have yy := h (xx)
+        exact yy
+        intro h BvC
+        cases BvC
+        case inl b => exact h.left b
+        case inr c => exact h.right c
+
+
 
 theorem eq7 {A B C : Prop} : C → (A ∧ B) ↔ (C → A) ∧ (C → B) :=
   sorry
